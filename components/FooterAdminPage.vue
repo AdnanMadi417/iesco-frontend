e7fb10be26533236bb528cfab93f908ab6dd9336
YounesDjelloul <younesdjelloul14@gmail.com>
2024-09-08 03:03:21 +0800

    footer positioning

--- components/FooterAdminPage.vue
+++ components/FooterAdminPage.vue
@@ -3,18 +3,21 @@
 </script>
 
 <template>
-<div class="footer">
-  <div>© 2024 – IESCO. All rights reserved.</div>
-</div>
+  <div class="footer">
+    <div>© 2024 – IESCO. All rights reserved.</div>
+  </div>
 </template>
 
 <style scoped>
-  .footer {
-    display: block;
-    background-color: var(--main-color);
-    text-align: center;
-    padding: 30px 0 ;
-    font-size: 28px;
-    color: #eeeeee;
-  }
+.footer {
+  display: block;
+  background-color: var(--main-color);
+  text-align: center;
+  padding: 30px 0;
+  font-size: 28px;
+  color: #eeeeee;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+}
 </style>
